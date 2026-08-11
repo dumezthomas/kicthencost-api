fe66100b5a9f84e288103ac1f9722fbab4628b4b
Thomas Dumez <dumezthomas@gmail.com>
2026-08-11 11:42:22 +0200

    initialize multi-module project

---
+++ api/src/main/java/dev/dumezthomas/kitchencost/KitchencostApiApplication.java
@@ -0,0 +1,14 @@
+package dev.dumezthomas.kitchencost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KitchencostApiApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(KitchencostApiApplication.class, args);
+    }
+
+}
